6b52f165d9885c24e0e21da93a39e1ec51743a60
Yoshiya Hinosawa <stibium121@gmail.com>
2024-07-04 20:52:21 +0900

    fix: issue 3

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export default function (a, b, bool) {
 	a = a.split('.');
 	b = b.split('.');
 
-	return fn(a[0], b[0]) || fn(a[1], b[1]) || (
+	return fn(a[0], b[0]) || fn(a[1], b[1]) || fn(a[2].split("-")[0], b[2].split("-")[0]) || (
 		b[2] = b.slice(2).join('.'),
 		bool = /[.-]/.test(a[2] = a.slice(2).join('.')),
 		bool == /[.-]/.test(b[2]) ? fn(a[2], b[2]) : bool ? -1 : 1
--- test/index.js
+++ test/index.js
@@ -24,6 +24,7 @@ test('compare :: less than', t => {
 	t.semiver('1.9.0', '1.9.1', -1);
 	t.semiver('1.0.0', '10.0.0', -1);
 	t.semiver('8.9.0', '10.0.0', -1);
+	t.semiver('1.0.0', '1.0.0-a', -1);
 	t.semiver('1.2.3-next.6', '1.2.3-next.10', -1);
 	t.semiver('2.0.0-alpha-6', '2.0.0-alpha-10', -1);
 	t.semiver('2.0.0-alpha.8', '2.0.0-beta.1', -1);
